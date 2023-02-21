ba4b868f837f4d22b2a324265c8989fa3efcbc76
katherine <pv.katherine7@gmail.com>
2023-02-21 20:13:10 +0000

    useState on Board

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.scss";
+import Game from "./components/Game/Game";
+
+const App = () => {
+  return (
+    <>
+      <h1>Tic Tac Toe</h1>
+      <Game />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Game/Game.jsx
@@ -0,0 +1,12 @@
+import "./Game.scss";
+import Board from "../Board/Board";
+
+const Game = () => {
+  return (
+    <div className="game-board">
+      <Board />
+    </div>
+  );
+};
+
+export default Game;
